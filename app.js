f0e04816b4377733229c0a9e4bc20b96a35c2662
Jason Serafica <seraficaj@gmail.com>
2021-04-04 19:07:16 -0700

    66. serving static files

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-//MIDDLEWARES
 const app = express();
+
+//MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log(`Hello from the middleware.`);
