5992bd1276a9a77c10e93ea3f53f783173739a6c
Jason Serafica <seraficaj@gmail.com>
2021-04-06 07:46:47 -0700

    set up connection to atlas db

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected succesfully!'));
 
 //START THE SERVER
 const port = process.env.PORT || 3000;
