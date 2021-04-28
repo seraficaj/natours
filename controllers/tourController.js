ea64d39a622342f16820242f35df37879a0b3bb1
Jason Serafica <seraficaj@gmail.com>
2021-04-28 07:06:20 -0700

    98. making the API better. pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,12 +23,24 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
-    //3 Field Limiting
+    //3) Field Limiting
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
     } else {
       query = query.select('-__v');
+    }
+
+    //4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = limit * (page - 1);
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist!');
     }
 
     //EXECUTE QUERY
