8a33c3c4f6a0d8d3a1bfd97b258f2c678f8588e5
Jason Serafica <seraficaj@gmail.com>
2021-04-28 07:15:55 -0700

    making the API better: aliasing

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,13 +3,17 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/')
-    .get(tourController.getAllTours)
-    .post(tourController.createTour);
+router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/:id')
-    .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
