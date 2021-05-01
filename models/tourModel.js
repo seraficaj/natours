0c888bf1041494152a9edc1de6ac518630b59407
Jason Serafica <seraficaj@gmail.com>
2021-04-30 17:03:17 -0700

    Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -63,6 +63,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -89,6 +93,19 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+//Query Middleware
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() -this.start} milliseconds`)
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
