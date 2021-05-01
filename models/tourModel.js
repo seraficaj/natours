c73c215157be0e5ddecf5e9e7588eabfa29f68dc
Jason Serafica <seraficaj@gmail.com>
2021-05-01 10:57:46 -0700

    106. Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -102,8 +102,16 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() -this.start} milliseconds`)
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  next();
+});
+
+//Aggregation Middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
   next();
 });
 
