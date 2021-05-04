3a88164a202ecca97fe3a704f9431943ee3be0fb
Jason Serafica <seraficaj@gmail.com>
2021-05-04 10:13:41 -0700

    118. handling invalid database ids

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,41 +1,45 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    error: err,
     message: err.message,
     stack: err.stack,
+    err,
   });
 };
 
 const sendErrorProd = (err, res) => {
-  //Operational, trusted error: send message to client.
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
-    //Programming or other unknown error: don't leak details!
-  } else {
-    //1) Log Error
-    console.error('ERROR ', err);
-
-    //2) Send generic message
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went very wrong.',
+  }
+  else {
+    console.error('💥 Error! 💥', err);
+    res.status(err.statusCode).json({
+      status: err.status, 
+      message: "There was an error, it's a problem from the server side! :(",
     });
   }
 };
 
 module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-
-  err.statusCode = err.statusCode || 500;
+  err.statusCode = err.statusCode || 500; 
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
-  } else if (process.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+  if (process.env.NODE_ENV == 'development') sendErrorDev(err, res);
+
+  else if (process.env.NODE_ENV == 'production') {
+    let error = Object.create(err);
+
+    if (err.name == 'CastError') error = handleCastErrorDB(err);
+    sendErrorProd(error, res);
   }
 };
