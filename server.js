b30a1c98ca226d4359cfd0cacd0359cfbce9de4d
Jason Serafica <seraficaj@gmail.com>
2021-05-05 11:19:45 -0700

    catching uncaught exceptions

--- server.js
+++ server.js
@@ -25,8 +25,16 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name);
   server.close(() => {
     process.exit(1);
   });
