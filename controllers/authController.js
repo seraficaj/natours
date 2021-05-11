0d48fee51aea9776cd2530f0b5b2b06664b8dd81
Jason Serafica <seraficaj@gmail.com>
2021-05-11 11:00:29 -0700

    131. protecting tour routes pt. 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -60,7 +61,6 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  console.log(token);
 
   if (!token) {
     return next(
@@ -68,10 +68,24 @@ exports.protect = catchAsync(async (req, res, next) => {
     );
   }
   // 2) Verification of token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3) Check if user still exists
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(
+      new AppError('The user belonging to this token no longer exists.', 401)
+    );
+  }
 
   // 4) Check if user changed password after token was issued
+  if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again!', 401)
+    );
+  }
 
+  //Grant access to protected route
+  req.user = freshUser;
   next();
 });
