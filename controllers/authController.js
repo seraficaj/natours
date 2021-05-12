a1c2b882890347a7ed5d572773c5a277f908a47c
Jason Serafica <seraficaj@gmail.com>
2021-05-12 11:27:22 -0700

    135. sending emails through nodemailer.

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -97,18 +98,42 @@ exports.restrictTo = (...roles) => (req, res, next) => {
     );
 };
 
-exports.forgotPassword = catchAsync (async (req,res,next) => {
+exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on POSTed email
-  const user = await User.findOne({email: req.body.email})
+  const user = await User.findOne({ email: req.body.email });
   if (!user) {
     return next(new AppError('There is no user with email address', 404));
   }
   // 2) Generate the random reset token
   const resetToken = user.createPasswordResetToken();
-  await user.save({ validateBeforeSave: false});
+  await user.save({ validateBeforeSave: false });
   // 3) Send it to the user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with new password and password confirm to: ${resetURL}\nIf you didn't forget your password, please ignore this password!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 minutes)',
+      message,
+    });
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email! Try again later!'),
+      500
+    );
+  }
 });
 
-exports.resetPassword = (req,rex,next) => {
-  
-}
+exports.resetPassword = (req, rex, next) => {};
