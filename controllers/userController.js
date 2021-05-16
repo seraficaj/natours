f804e5c4f8ee41f826a9f12a6ca2e5e14f5cf000
Jason Serafica <seraficaj@gmail.com>
2021-05-16 14:33:30 -0700

    141. sending a JWT via cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,6 @@
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
