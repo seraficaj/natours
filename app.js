b7d4b95d4df0b471a2b36af5c032fcf4f4d94882
Jason Serafica <seraficaj@gmail.com>
2021-05-18 13:41:33 -0700

    144. data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +28,12 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP. Please try again in an hour!',
 });
 app.use('/api', limiter);
+
+// data sanitzation against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitzation against XSS
+app.use(xss());
 
 // Body parser, reading data from the body into req.body
 app.use(
