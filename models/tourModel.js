b3243e7e6c5acaf2f7819a9465be3e271203aa82
Jason Serafica <seraficaj@gmail.com>
2021-05-21 21:26:55 -0700

    152. populating tour guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -160,7 +160,7 @@ tourSchema.pre(/^find/, function(next) {
     select: '-__v -passwordChangedAt',
   });
   next();
-}
+});
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
