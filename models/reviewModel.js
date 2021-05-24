7d448456a6cd43f2826b83d6fa6f72dcd0dd1b02
Jason Serafica <seraficaj@gmail.com>
2021-05-23 17:08:18 -0700

    155. populating reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,6 +32,20 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  });
+
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
