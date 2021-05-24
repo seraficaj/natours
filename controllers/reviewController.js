cb1f51f933c161eb667c324c68c1f70756915724
Jason Serafica <seraficaj@gmail.com>
2021-05-24 12:06:49 -0700

    157. implementing simple nested routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,25 +1,29 @@
 const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getAllReviews = catchAsync(async(req, res, next) => {
+exports.getAllReviews = catchAsync(async (req, res, next) => {
   const reviews = await Reviews.find();
 
   res.status(200).json({
     status: 'success',
     results: reviews.length,
     data: {
-      reviews
-    }
-  })
+      reviews,
+    },
+  });
 });
 
-exports.createReview = catchAsync(async(req, res, next) => {
+exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
-  
+
   res.status(201).json({
     status: 'success',
     data: {
-      review: newReview
-    }
-  })
+      review: newReview,
+    },
+  });
 });
