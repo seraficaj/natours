fa38a5d79048d4bb5fdd374544b0fc9a770978c0
Jason Serafica <seraficaj@gmail.com>
2021-06-01 19:40:56 -0700

    176. first steps with pug

--- app.js
+++ app.js
@@ -72,7 +72,10 @@ app.use((req, res, next) => {
 
 //ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Magical Forest hiker',
+    user: 'JONAS',
+  });
 })
 
 app.use('/api/v1/tours', tourRouter);
