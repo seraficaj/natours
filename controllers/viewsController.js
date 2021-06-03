0073fb62d48a1f31a9809d8e1d591460994ffb6a
Jason Serafica <seraficaj@gmail.com>
2021-06-03 10:40:46 -0700

    183. building tour page pt. 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Ice Cream Taster',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get tour data, incl. guides, reviews, etc.
+  const tour = await (await Tour.findOne({ slug: req.params.slug })).populate({
+    path: 'reviews',
+    select: 'review rating user',
   });
-};
+  // 2) Build template
+
+  // 3) Render template using data from 1)
+
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
