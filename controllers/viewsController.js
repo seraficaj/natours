a4ff2b2cd000014ade8d479b02783302658c661d
Jason Serafica <seraficaj@gmail.com>
2021-06-03 19:58:23 -0700

    186. including a map with mapbox pt. 2

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -5,7 +5,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
   // 2) Build template
-  
+
   // 3) Render template using tour data
   res.status(200).render('overview', {
     title: 'All Tours',
@@ -23,8 +23,14 @@ exports.getTour = catchAsync(async (req, res) => {
 
   // 3) Render template using data from 1)
   console.log(tour);
-  res.status(200).render('tour', {
-    title: tour.name,
-    tour,
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('tour', {
+      title: tour.name,
+      tour,
+    });
 });
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -2,3 +2,47 @@
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2VyYWZpY2FqIiwiYSI6ImNrcGhwZHYwZDByNzAydW85dmExYTY4a2EifQ.M7XF43ELi_wcupoL0tVv5w';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/seraficaj/ckphpyha209xo17mmncs1dw7o',
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+  // Add Marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day} ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
