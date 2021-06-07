15ef582b1cdf5153ca3f02e042f762cf3ec736ac
Jason Serafica <seraficaj@gmail.com>
2021-06-07 16:48:01 -0700

    190. logging in users with our api

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -12,21 +14,12 @@ const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      alert('Logged in succesfully!');
+      showAlert('success', 'Logged in succesfully!');
       window.setTimeout(() => {
         location.assign('/')
       }, 500);
     }
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  login(email, password);
-});
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,48 +1,46 @@
 /* eslint-disable */
+export const displayMap = (locations) => {
+  mapboxgl.accessToken =
+    'pk.eyJ1Ijoic2VyYWZpY2FqIiwiYSI6ImNrcGhwZHYwZDByNzAydW85dmExYTY4a2EifQ.M7XF43ELi_wcupoL0tVv5w';
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/seraficaj/ckphpyha209xo17mmncs1dw7o',
+    scrollZoom: false,
+  });
 
-mapboxgl.accessToken =
-  'pk.eyJ1Ijoic2VyYWZpY2FqIiwiYSI6ImNrcGhwZHYwZDByNzAydW85dmExYTY4a2EifQ.M7XF43ELi_wcupoL0tVv5w';
+  const bounds = new mapboxgl.LngLatBounds();
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/seraficaj/ckphpyha209xo17mmncs1dw7o',
-  scrollZoom: false,
-});
+  locations.forEach((loc) => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+    // Add Marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom',
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-const bounds = new mapboxgl.LngLatBounds();
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day} ${loc.description}</p>`)
+      .addTo(map);
 
-locations.forEach((loc) => {
-  // Create marker
-  const el = document.createElement('div');
-  el.className = 'marker';
-  // Add Marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom',
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
 
-  // Add popup
-  new mapboxgl.Popup({
-    offset: 30,
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day} ${loc.description}</p>`)
-    .addTo(map);
-
-  // Extend map bounds to include current location
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100,
-  },
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100,
+    },
+  });
+};
