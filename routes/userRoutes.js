a4170b9a83fc06873d21faa7860ea91cedf82691
Jason Serafica <seraficaj@gmail.com>
2021-07-15 19:25:29 -0700

    198. image upload using multer: users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const multer = require('multer');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+
+const upload = multer({ dest: 'public/img/users'});
 
 const router = express.Router();
 
@@ -11,13 +14,14 @@ router.get('/logout', authController.logout);
 router.post('/forgotPassword', authController.login);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 
 router.get('/me', userController.getMe, userController.getUser);
 
-router.patch('/updateMe', userController.updateMe);
+router.patch('/updateMe', upload.single('photo'), userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
